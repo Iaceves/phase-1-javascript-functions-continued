715058ea45c6f77c64ce04d440bc5d83ebe712f8
iaceves <acevesaceves97@gmail.com>
2022-08-11 22:19:11 -0400

    commit tree 492faa4b79c1e7c5fee42a473fdb2e962389c7e4

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // code your solution here
+function saturdayFun(saturday = 'roller-skate'){
+    return (`This Saturday, I want to ${saturday}!`);
+}
+
+function mondayWork(monday = 'go to the office'){
+    return (`This Monday, I will ${monday}.`);
+}
+
+function wrapAdjective(a="*"){
+    let sentence
+    return function(b = "a hard worker"){
+        if (a === "*"){
+            sentence = (`You are ${a}${b}${a}!`)
+        }
+    
+        else if (a === "||"){
+            sentence = (`You are ${a}${b}${a}!`)
+        }
+        return sentence
+    }
+}
+
